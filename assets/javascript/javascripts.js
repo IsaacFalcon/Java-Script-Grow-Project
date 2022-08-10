d2ad8b8c7a69e38d62afd71cce65a81f3b421a0b
Isaac Falcon <ice6490@gmail.com>
2022-08-10 16:35:14 -0500

    new index and java

---
+++ assets/javascript/javascripts.js
@@ -0,0 +1,36 @@
+document.getElementById("button3").addEventListener("click", function(){
+
+    document.getElementById("box").style.opacity= "0.5";
+
+
+});
+      document.getElementById("button2").addEventListener("click", function(){
+
+       document.getElementById("box").style="height:150px; width:150px; background-color:blue; margin:25px; opacity:1";
+
+
+      });
+           document.getElementById("button4").addEventListener("click", function(){
+
+             document.getElementById("box").style="height:150px; width:150px; background-color:orange; margin:25px; opacity:1";
+
+
+            });
+                document.getElementById("button1").addEventListener("click", function(){
+
+                  document.getElementById("box").style.height = "500px";
+
+
+                })
+                document.getElementById("button5").addEventListener("click", function(){
+
+              document.getElementById("box").style="height:150px; width:150px; background-color:red; margin:25px; opacity:1";
+
+
+            });
+         document.getElementById("button6").addEventListener("click", function(){
+
+     document.getElementById("box").style="height:150px; width:150px; background-color:purple; margin:25px; opacity:1";
+
+
+});
